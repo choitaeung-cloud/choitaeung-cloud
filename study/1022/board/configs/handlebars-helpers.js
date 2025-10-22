59899db3dcdbbbec34345d58ee451c70719d2a8e
Choitaeung <choitaeung990221@gmail.com>
2025-10-22 14:39:01 +0900

    add board

---
+++ study/1022/board/configs/handlebars-helpers.js
@@ -0,0 +1,7 @@
+const { eq } = require("lodash");
+
+module.exports = {
+    lengthOfList: (List = []) => List.length,
+    eq: (val1, val2) => val1 === val2,
+    dateString: (isoString) => new Date(isoString)
+}
